26731ffb306c130328e0fce9c059e8571ff3a3c4
matheus <105015473+math4798@users.noreply.github.com>
2023-10-21 14:06:27 -0300

    Update stack.routes.tsx

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -10,7 +10,7 @@ export default function StackRoutes(){
     return(
             <Navigator
                 screenOptions={{
-                    headerShown: true,
+                    headerShown: false,
                 }}
             >
                 <Screen 
