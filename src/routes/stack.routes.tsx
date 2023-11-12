e755ff8f15184d3c92f10dfc1a2ab7c4f19d9f95
Alexandre de Oliveira Pinheiro <zxandnaxx@gmail.com>
2023-11-12 16:33:15 -0300

    Att

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,58 +1,53 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Tabnavigation from "./Tabnavigation.route";
 import RegisterPage from "../screens/RegisterPage";
-import AuthPage from "../screens/AuthPage";
 import ResetPage from "../screens/ResetPage";
 import ConfigPage from "../screens/ConfigPage";
-import Program from "../screens/ProgramPage";
 import RegisteredPage from "../screens/RegisteredPage";
 import HistoricPage from "../screens/HistoricPage";
 import NewRegistrationPage from "../screens/NewRegistrationPage";
-const {Screen, Navigator}= createNativeStackNavigator();
+import ProgramPage from "../screens/ProgramPage";
+const { Screen, Navigator } = createNativeStackNavigator();
 
-export default function StackRoutes(){
-    return(
-            <Navigator
-                screenOptions={{
-                    headerShown: false,
-                }}
+export default function StackRoutes() {
+    return (
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
             >
-                <Screen 
-                    name="AuthPage" 
-                    component={AuthPage}
-                />
-                <Screen 
-                    name="RegisterPage" 
-                    component={RegisterPage}
-                />
-                <Screen 
-                    name="ResetPage" 
-                    component={ResetPage}
+                <Screen
+                name="Tabnavigation"
+                component={Tabnavigation}
             />
-                <Screen 
-                    name="Program" 
-                    component={Program}
-            />     
-                <Screen 
-                    name="Tabnavigation" 
-                    component={Tabnavigation}
-            />      
-                <Screen 
-                    name="RegisteredPage" 
-                    component={RegisteredPage}
-            />         
-            <Screen 
-                    name="ConfigPage" 
-                    component={ConfigPage}
+            <Screen
+                name="ProgramPage"
+                component={ProgramPage}
             />
-                <Screen 
-                    name="HistoricPage" 
-                    component={HistoricPage}
-            />     
-                <Screen 
-                    name="NewRegistrationPage" 
-                    component={NewRegistrationPage}
-            />     
+            <Screen
+                name="RegisterPage"
+                component={RegisterPage}
+            />
+            <Screen
+                name="ResetPage"
+                component={ResetPage}
+            />
+            <Screen
+                name="RegisteredPage"
+                component={RegisteredPage}
+            />
+            <Screen
+                name="ConfigPage"
+                component={ConfigPage}
+            />
+            <Screen
+                name="HistoricPage"
+                component={HistoricPage}
+            />
+            <Screen
+                name="NewRegistrationPage"
+                component={NewRegistrationPage}
+            />
         </Navigator>
 
 
