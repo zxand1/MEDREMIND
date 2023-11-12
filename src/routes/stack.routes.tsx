bd9713d9c83e19f77c87f11d29847d0ad6c4f7be
Alexandre de Oliveira Pinheiro <zxandnaxx@gmail.com>
2023-11-12 17:12:27 -0300

    att

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,7 +1,5 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Tabnavigation from "./Tabnavigation.route";
-import RegisterPage from "../screens/RegisterPage";
-import ResetPage from "../screens/ResetPage";
 import ConfigPage from "../screens/ConfigPage";
 import RegisteredPage from "../screens/RegisteredPage";
 import HistoricPage from "../screens/HistoricPage";
@@ -23,14 +21,6 @@ export default function StackRoutes() {
             <Screen
                 name="ProgramPage"
                 component={ProgramPage}
-            />
-            <Screen
-                name="RegisterPage"
-                component={RegisterPage}
-            />
-            <Screen
-                name="ResetPage"
-                component={ResetPage}
             />
             <Screen
                 name="RegisteredPage"
